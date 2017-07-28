a69a9a4d316d72cb664049232f7f6e058286bedd
mib1092 <mazurenko.ihor@gmail.com>
2017-07-28 16:04:24 +0300

    mib

--- assets/js/custom/scripts.js
+++ assets/js/custom/scripts.js
@@ -1,14 +1,12 @@
-$(function(){
+jQuery(function($){
 
     // for smooth scroll
-	if ( $('a').is('.smooth-scroll') ) {
-        smoothScroll.init({
-            selector: '.smooth-scroll', // Selector for links (must be a class, ID, data attribute, or element tag)
-            speed: 500, // Integer. How fast to complete the scroll in milliseconds
-            easing: 'easeInQuad', // Easing pattern to use
-            offset: 130 // Integer. How far to offset the scrolling anchor location in pixels
-        });
-    }
+    smoothScroll.init({
+        selector: '.smooth-scroll', // Selector for links (must be a class, ID, data attribute, or element tag)
+        speed: 500, // Integer. How fast to complete the scroll in milliseconds
+        easing: 'easeInQuad', // Easing pattern to use
+        offset: 130 // Integer. How far to offset the scrolling anchor location in pixels
+    });
 
 	$(document).ready(function() {
 	    //for reviews slider
@@ -44,6 +42,7 @@ $(function(){
                 }
             })
         }
+
         if($('#f-date').length) {
             $( "#f-date" ).datepicker({
                 dateFormat: "d MM yy",
