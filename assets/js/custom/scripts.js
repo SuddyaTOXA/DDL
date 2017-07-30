3ba08c6c26fbf1d9d9a2e3debfe3c055fdcc1a70
mib1092 <mazurenko.ihor@gmail.com>
2017-07-31 02:28:20 +0300

    mib

--- assets/js/custom/scripts.js
+++ assets/js/custom/scripts.js
@@ -22,26 +22,34 @@ jQuery(function($){
         }
 
         //for iphone-inline-video
+        enableInlineVideo($('video').get(0), {
+            iPad: true
+        });
         if ($('.autoplay').length) {
             var videoBox = $('video.autoplay'),
-                video = videoBox.get(0);
-                enableInlineVideo(video, {
-                    iPad: true
-                });
-                setTimeout(function () {
-                    video.play();
-                    videoBox.addClass('v-play');
-                }, 200);
-            videoBox.on('click', function () {
-                if($(this).hasClass('v-play')) {
-                    video.pause();
-                    $(this).removeClass('v-play');
-                } else {
-                    video.play();
-                    $(this).addClass('v-play');
-                }
-            })
+                video = videoBox.get(0),
+                speed = videoBox.data('speed');
+
+            setTimeout(function () {
+                video.play();
+                videoBox.addClass('v-play');
+                if (speed) video.playbackRate = speed;
+            }, 200);
         }
+        $('.section-hero video').on('click', function () {
+            var video = $(this).get(0),
+                speed = $(this).data('speed');
+
+            if($(this).hasClass('v-play')) {
+                video.pause();
+                $(this).removeClass('v-play');
+            } else {
+                video.play();
+                $(this).addClass('v-play');
+                if (speed) video.playbackRate = speed;
+            }
+        });
+
 
         if($('#f-date').length) {
             $( "#f-date" ).datepicker({
